c1bc495e6cc7fd5585343fd64b06fcd3b65fac5f
Swoodya <44286193+Swoodya@users.noreply.github.com>
2018-12-11 08:39:54 +0100

    Update Lion.cs

--- Lion.cs
+++ Lion.cs
@@ -8,6 +8,14 @@ namespace TP_POO_Héritage
 {
     class Lion : Felin
     {
+        public Lion(string nom, string lieuHabitation, string monCrie, bool jeSuisDomestique, int nombrePattes)
+            : base(nom, lieuHabitation, monCrie, jeSuisDomestique,nombrePattes)
+        {
 
+        }
+        public void Afficher()
+        {
+            Console.WriteLine("nom : {0}, lieu Habitation : {1}, son Crie : {2}, jeSuisDomestique : {3}, nombrePattes : {4}", this.nom, this.lieuHabitation, this.monCrie, this.jeSuisDomestique, this.nombrePattes);
+        }
     }
 }
